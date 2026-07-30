c3692a26fa0e4a7f00b3122528766637fb4eaaf5
varad raju takale <varadtakale1706@gmail.com>
2026-07-30 15:59:39 +0530

    [LeetCode] 204. Count Primes (Medium)

---
+++ Medium/204-count-primes.java
@@ -0,0 +1,33 @@
+// 204. Count Primes (Medium)
+// https://leetcode.com/problems/count-primes/
+// Runtime: 102 ms  Memory: 51.2 MB
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+
+    
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
