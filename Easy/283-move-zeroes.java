9a2c73719a15f4158c657442f09da0dda8c84917
varad raju takale <varadtakale1706@gmail.com>
2026-09-04 11:34:00 +0530

    [LeetCode] 283. Move Zeroes (Easy)

--- Easy/283-move-zeroes.java
+++ Easy/283-move-zeroes.java
@@ -1,9 +1,9 @@
 // 283. Move Zeroes (Easy)
 // https://leetcode.com/problems/move-zeroes/
-// Runtime: 2 ms  Memory: 47.8 MB
+// Runtime: 3 ms  Memory: 47.7 MB
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0 ;
+       /* int j = 0 ;
         for(int i = 0; i<nums.length; i = i+1){
             if(nums[i] != 0){
                 nums[j] = nums[i];
@@ -14,6 +14,16 @@ class Solution {
         while(j < nums.length){
             nums[j] = 0;
             j = j+1;
+        }*/
+
+        int j = 0;
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] != 0 ){
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[j] = temp;
+                j = j+1;
+            }
         }
     }
 }
