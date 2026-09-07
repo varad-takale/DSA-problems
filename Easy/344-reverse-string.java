970d995ad69b1e248884e7994d35a53be3d15c9b
varad raju takale <varadtakale1706@gmail.com>
2026-09-07 11:50:01 +0530

    [LeetCode] 344. Reverse String (Easy)

---
+++ Easy/344-reverse-string.java
@@ -0,0 +1,21 @@
+// 344. Reverse String (Easy)
+// https://leetcode.com/problems/reverse-string/
+// Runtime: 0 ms  Memory: 48.2 MB
+class Solution {
+    public void reverseString(char[] s) {
+
+        int i = 0;
+        int j = s.length-1;
+
+        while( i <= j){
+
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--; 
+        }
+        
+    }
+}
